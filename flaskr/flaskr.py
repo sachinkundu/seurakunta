992d0136ae909408f4b98abb3c1b080b07aa3dfb
Laura Meskanen-Kundu <laura.meskanen@gmail.com>
2017-08-22 23:12:42 +0100

    added carousel, fixed styling issues, added old javascript functionality

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,21 +1,22 @@
 import json
 import arrow
 import time
-import schedule
 import feedparser
 from flask import g
 from flask import Flask, render_template, current_app
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
-          7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+months = {1: 'Tammikuuta', 2: 'Helmikuuta', 3: 'Maaliskuuta', 4: 'Huhtikuuta', 5: 'Toukokuuta', 6: 'Kesäkuuta', 7: 'Heinäkuuta', 8: 'Elokuuta', 9: 'Syyskuuta', 10: 'Lokakuuta', 11: 'Marraskuuta', 12: 'Joulukuuta'}
 
 
 def humanize_date(date):
     arrowed = arrow.get(date)
     return str(arrowed.day) + ' ' + str(months[arrowed.month]) + ' ' + str(arrowed.year)
 
+def get_time(date):
+    t = arrow.get(date)
+    return t.format('HH:mm')
 
 app = Flask(__name__)
 scheduler = BackgroundScheduler()
@@ -42,5 +43,5 @@ def hello():
     entries = [entry for entry in feed.entries if arrow.get(entry.updated) < max_date]
     for entry in entries:
         ret.append([entry.title, entry.summary.replace(
-            "&nbsp;", ""), humanize_date(entry.updated)])
+            "&nbsp;", ""), humanize_date(entry.updated), get_time(entry.updated)])
     return render_template('main.html', output=ret)
--- flaskr/static/js/main.js
+++ flaskr/static/js/main.js
@@ -1,9 +1,42 @@
 $( document ).ready(function() {
-    //calling the first function for fetching event data:
-    // weeksEvents();
-  
+
       var clock = $('.clock').FlipClock({
           clockFace: 'TwentyFourHourClock'
       });
-  
+
+      $('.media-object').error(function(){
+        $(this).attr('src', 'static/img/logo.png');
+        console.log('error?');
+      });
+
+      /* Looping through events */
+      var numberOfdataEements = $('.singleEvent').length;
+      var timeInterval = 14000 * numberOfdataEements;
+
+      if(numberOfdataEements > 1){
+        displayScreenLoop();
+      }
+
+      function displayScreenLoop(numberOfdataEements){
+        setInterval(function(){
+          console.log("time interval is : " + timeInterval);
+          slideshowLoop();
+        }, timeInterval);
+
+        function slideshowLoop(){
+          $('#eventTiles .singleEvent').each(function(index) {
+            var $div = $(this);
+            setTimeout(function() {
+              $div.addClass('grow').delay(10000).queue(function(next){
+                $div.addClass('shrink').delay(1000).queue(function(next){
+                  $div.removeClass('grow shrink');
+                  next();
+                });
+                next();
+              });
+            }, 11000 * index);
+          });
+        }
+      }
+
   });
